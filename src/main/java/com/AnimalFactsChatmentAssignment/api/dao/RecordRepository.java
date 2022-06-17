3e4416da902dcb846d5c938cf2a1b1e76a17790d
nitingupta11 <niting1200.ng@gmail.com>
2022-06-17 20:00:53 +0530

    Reinit repo

--- src/main/java/com/AnimalFactsChatmentAssignment/api/dao/RecordRepository.java
+++
@@ -1,5 +0,0 @@
-package com.AnimalFactsChatmentAssignment.api.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<com.AnimalFactsChatmentAssignment.api.model.Record, Long> {}
--- src/main/java/com/AnimalFactsChatmentAssignment/api/data/Animals.java
+++
@@ -1,5 +0,0 @@
-package com.AnimalFactsChatmentAssignment.api.data;
-
-public enum Animals {
-	CAT, DOG
-}
